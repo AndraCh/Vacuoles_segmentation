9b8fc09f097467b112826821ee2e87b8b0627f44
Andra Ch <andra.chincisan@gmail.com>
2018-10-09 16:11:24 +0200

    Commit message

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Vacuoles_segmentation
 Matlab functions to segment and quantify vacuoles in histological images.
+
+The algorithm was developed in MATLAB R2016B, using the Image Processing toolbox. 
+Image segmentation was performed using Otsu’s thresholding method (T=0.7). 
+Only round vacuoles with an area in the range of [200 pixels, 2000 pixels] and with a shape values > 0.9 
+(where shape was calculated as shape = (4*PI*Area) / (Perimeter^2)) were quantified. 
+
+
--- vacuoles_quantification.m
+++ vacuoles_quantification.m
@@ -5,24 +5,40 @@ close all;
 clc;
 clear all;
 %% Read image
-path_dir = 'path_name'
-path_save = 'save_path_name'
+path_dir = 'path_dir\'
+path_save = 'path_save\'
+path_xls = 'path_save'
+save_xls = 'file.xls'
 image_type = '.tif';
 srcFiles = dir(strcat(path_dir, '*', image_type));
 for i = 1 : length(srcFiles)
+    close all;
+    % Read image
     image = imread (strcat(path_dir,srcFiles(i).name)); 
+    % Gray image
     imageGray = getgrayimage (image);
+    % Segment image
     imageBinary = segmentimage (imageGray);
-    %figure, imshow(image(:,:,3)); title('imageVacoulesNew');
+    figure, imshow(image(:,:,3)); title('imageVacoulesNew');
+    % Get vacuoles
     imageVacoules = getvacoulesimage (imageBinary);
-    [numberOfVacoules,sumAreaVacoules,sumPerimeterVacoules, Area_all,Eccentricity_all] = quantifyvacoules (imageVacoules);
+    % Process vacuoles
     imageVacoulesNew= remove_vessels (imageGray, imageVacoules);
-    %figure, imshow(imageVacoulesNew); title('imageVacoulesNew');
-
+    % Quantify vacuoles
+    [numberOfVacoules,sumAreaVacoules,sumPerimeterVacoules, Area_all,Eccentricity_all] = quantifyvacoules (imageVacoulesNew);
+    figure, imshow(imageVacoulesNew); title('imageVacoulesNew');
+    % Fuse original and segmented image
     C = imfuse(imageGray,imageVacoulesNew);
     figure, imshow(C); title(strcat(path_dir,srcFiles(i).name));
+    % Save fused image
     imwrite(mat2gray(C),strcat(path_save, srcFiles(i).name));
+    name_image =string (srcFiles(i).name);
+    % Create arry with data for each each
+    all_param(i, :) = [{char(name_image)} numberOfVacoules sumAreaVacoules sumPerimeterVacoules Area_all]
 end
+% Save data in Excel file
+xlswrite(strcat (path_xls, '\het_cortex'), save_xls)
+
 %% Get second layer of the RBG image (blue layer) as gray image
 function imageGray = getgrayimage (image)
 imageGray = image(:,:,2);
@@ -32,8 +48,8 @@ end
 %% Threshold segmentation
 function imageBinary = segmentimage (imageGray)
 % Adjust image contrast
-imageGray = imadjust(imageGray,[0.4 0.6],[]);
-% Segment image
+imageGray = imadjust(imageGray,[0.2 0.8],[]);  
+% Binarize image
 imageBinary = im2bw(imageGray,0.7);
 % Remove small objects
 imageBinary = bwareaopen(imageBinary,20);
@@ -57,9 +73,6 @@ S = regionprops(imageCC, 'Area', 'Perimeter', 'Eccentricity', 'EquivDiameter');
 imageLabel = labelmatrix(imageCC);
 % Calculate object's shape
 shape = 4*pi*[S.Area]./power ([S.Perimeter],2);
-%shape = [S.Perimeter].^2 ./(4*pi*[S.Area])
-% Select all large objects Area> 350 pixels
-%imgBinaryObjLarge = ismember(imageLabel, find(([S.Area] <=  500))); 
 % Select smaller and rounder objects
 imgBinaryObjSmall = ismember(imageLabel, find(([S.Area] >= 200) & ([S.Area]< 2000) &(shape>= 0.9)));% & ([S.Eccentricity]>0.5)));
 % Combine images
@@ -68,22 +81,30 @@ imageVacoules = imgBinaryObjSmall;
 end
 
 %% Final objects list
-function [numberOfVacoules,sumAreaVacoules,sumPerimeterVacoules,Area_all,Eccentricity_all] = quantifyvacoules (imageVacoules)
+function [numberOfVacoules,sumAreaVacoules,sumPerimeterVacoules,mean_area_all,mean_eccentricity_all] = quantifyvacoules (imageVacoules)
 [imageLabel, numberOfVacoules] = bwlabel(imageVacoules);
  imageCC = bwconncomp(imageVacoules, 4 );
 % Geometrical parameters of the image
 S = regionprops(imageCC, 'Area', 'Perimeter', 'Eccentricity'); 
+% Calculate sum of all vacuole areas in an image
 sumAreaVacoules = sum([S.Area]);
+% Calculate sum of all vacuole perimeters in an image
 sumPerimeterVacoules = sum([S.Perimeter]);
-Area_all = [S.Area];
-Eccentricity_all= [S.Eccentricity];
+% Calculate mean area of vacuole in an image
+mean_area_all = mean([S.Area]);
+% Calculate mean eccentricity of vacuole in an image
+mean_eccentricity_all= mean([S.Eccentricity]);
 end
 
 function mat = window_image(Cx,Cy,D,imageGray)
 [max_n, max_m] = size(imageGray);
-D = D /2;
+D = D/3;
 new_coordinates = [round(Cx-D/2),round(Cx+D/2),round(Cy-D/2),round(Cy+D/2)];
-
+for i=1:length(new_coordinates)
+    if new_coordinates(i) <=0
+        new_coordinates(i) = 0;  
+    end
+end
 if (Cx-D)>=0 & (Cx-D)<=max_m
     new_coordinates(1) = round(Cx-D);
 end
@@ -107,6 +128,14 @@ end
 end
 
 function [imageVacoules]= remove_vessels (imageGray, imageVacoules)
+% Create a small image for each of the initial objects that are considered
+% vacuoles. If dark regions appear in the close proximity of an a object it 
+% means that there is a vessel close to the object and that this object is 
+% not a real vacuole, so it should be remove from the vacuoles list.
+% Compare the average intensity of small images with a threshold T =
+% 150. Objects with an average intensity <150 will be removed.
+ 
+% Image parameters
 S = regionprops(imageVacoules, 'centroid', 'MajorAxisLength'); 
 centroids = cat(1, S.Centroid);
 diameters = [S.MajorAxisLength];
@@ -117,10 +146,11 @@ for noObj=1:numberOfObject
      mat = window_image(centroids(noObj,1),centroids(noObj,2),diameters(noObj),imageGray);
      [m, n] = size(mat);
      if (m>10) & (n>10)
-        if mean(mean(mat))<100%mean(mean(imageGray))70
+        if mean(mean(mat))<150
             imageVacoules(imgLabel==noObj) = 0;
         end
      end
 end
+% Count objects
 [imgLabel, numberOfObject] = bwlabel(imageVacoules);
 end
